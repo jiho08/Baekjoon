cd58da1cb51495b4a7a0b4923f7d2b1e06d9f6a2
박지호 <57872222k@gmail.com>
2024-03-25 15:37:07 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("         ,r\'\"7");
+            Console.WriteLine("r`-_   ,\'  ,/");
+            Console.WriteLine(" \\. \". L_r\'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
