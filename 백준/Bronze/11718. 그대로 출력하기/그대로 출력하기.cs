f8f083d8e9b447dd378a7d24b8c16da557534901
박지호 <57872222k@gmail.com>
2024-04-04 12:59:27 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 64 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,10 @@
+bool b = true;
+            while (b == true)
+            {
+                string s = Console.ReadLine();
+                Console.WriteLine(s);
+                if (s == null)
+                {
+                    b = false;
+                }
+            }
