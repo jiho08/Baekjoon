17d6df02105a0f53b7edd8b180ed1c333d70576f
박지호 <57872222k@gmail.com>
2024-04-18 21:16:02 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 64 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,6 @@
+int[] n1 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            for (int i = 0; i < n1[0]; i++)
+            {
+                int[] n2 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            }
+            Console.WriteLine("비와이");
