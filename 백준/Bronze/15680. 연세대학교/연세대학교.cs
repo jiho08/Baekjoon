d68fc3a948ea7072d4af713e57d80259c8b27b60
박지호 <57872222k@gmail.com>
2024-04-19 23:11:00 +0900

    [Bronze V] Title: 연세대학교, Time: 60 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cs
@@ -0,0 +1,9 @@
+int n = int.Parse(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine("YONSEI");
+            }
+            else
+            {
+                Console.WriteLine("Leading the Way to the Future");
+            }
