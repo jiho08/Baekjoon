9dbd307a5312da4ff5b983d37a579239cccb6890
박지호 <57872222k@gmail.com>
2024-04-21 23:47:44 +0900

    [Bronze V] Title: 자동완성, Time: 60 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cs
@@ -0,0 +1,9 @@
+string s = Console.ReadLine();
+            if (s == "N" || s == "n")
+            {
+                Console.WriteLine("Naver D2");
+            }
+            else
+            {
+                Console.WriteLine("Naver Whale");
+            }
