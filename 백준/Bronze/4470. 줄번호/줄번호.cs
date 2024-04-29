c528a835649cd1df324dfe239ececd9bc93665ee
박지호 <57872222k@gmail.com>
2024-04-29 17:20:49 +0900

    [Bronze IV] Title: 줄번호, Time: 68 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cs
@@ -0,0 +1,6 @@
+int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                string s = Console.ReadLine();
+                Console.WriteLine($"{i}. {s}");
+            }
