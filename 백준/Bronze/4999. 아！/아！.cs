f702f299c33cdae5fdde9aa4d4c848dfa9d920a6
박지호 <57872222k@gmail.com>
2024-05-03 23:43:07 +0900

    [Bronze V] Title: 아!, Time: 60 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cs
@@ -0,0 +1,11 @@
+string a = Console.ReadLine();
+        string b = Console.ReadLine();
+
+        if (a.Length >= b.Length)
+        {
+            Console.WriteLine("go");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
