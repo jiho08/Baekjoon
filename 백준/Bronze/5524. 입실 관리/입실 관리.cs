4c3509f7a9e97ee2b81083cff8a006ca81fcee07
박지호 <57872222k@gmail.com>
2024-05-20 23:22:36 +0900

    [Bronze IV] Title: 입실 관리, Time: 68 ms, Memory: 5528 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.cs
@@ -0,0 +1,7 @@
+int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s.ToLower());
+        }
