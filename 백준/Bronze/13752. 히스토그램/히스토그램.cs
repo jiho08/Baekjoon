ada3e336a740a2d901bf05aa0dcebbf7b4d8691d
박지호 <57872222k@gmail.com>
2024-06-05 23:32:35 +0900

    [Bronze IV] Title: 히스토그램, Time: 432 ms, Memory: 6600 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cs
@@ -0,0 +1,10 @@
+int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < a; j++)
+                Console.Write("=");
+            Console.WriteLine();
+        }
