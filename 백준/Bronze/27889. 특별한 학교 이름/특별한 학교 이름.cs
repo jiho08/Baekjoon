93c48afd0efcef80e8d2a7097863455f614358f5
박지호 <57872222k@gmail.com>
2024-07-07 23:53:54 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 60 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cs
@@ -0,0 +1,17 @@
+string s = Console.ReadLine();
+
+            switch (s)
+            {
+                case "NLCS":
+                    Console.Write("North London Collegiate School");
+                    break;
+                case "BHA":
+                    Console.Write("Branksome Hall Asia");
+                    break;
+                case "KIS":
+                    Console.Write("Korea International School");
+                    break;
+                case "SJA":
+                    Console.Write("St. Johnsbury Academy");
+                    break;
+            }
