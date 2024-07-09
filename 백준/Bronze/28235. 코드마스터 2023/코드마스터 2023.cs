12b0a8c034193d7e7b136e50a1334b64ea11d37f
박지호 <57872222k@gmail.com>
2024-07-10 00:07:39 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 60 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,17 @@
+string s = Console.ReadLine();
+
+            switch (s)
+            {
+                case "SONGDO":
+                    Console.Write("HIGHSCHOOL");
+                    break;
+                case "CODE":
+                    Console.Write("MASTER");
+                    break;
+                case "2023":
+                    Console.Write("0611");
+                    break;
+                case "ALGORITHM":
+                    Console.Write("CONTEST");
+                    break;
+            }
