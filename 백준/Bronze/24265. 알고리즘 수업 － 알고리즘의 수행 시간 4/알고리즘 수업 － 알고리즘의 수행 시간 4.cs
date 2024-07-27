770689c12eb6f3f4b0904c5261bf7f79b65acae5
박지호 <57872222k@gmail.com>
2024-07-27 19:31:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 64 ms, Memory: 5524 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cs
@@ -0,0 +1,8 @@
+long n = long.Parse(Console.ReadLine());
+            long s = 0;
+
+            for (int i = 1; i <= n - 1; ++i)
+                s += i;
+
+            Console.WriteLine(s);
+            Console.WriteLine(2);
