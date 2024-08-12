050f256529a6335f2661b3cb2ea6af303d84c98e
박지호 <57872222k@gmail.com>
2024-08-12 09:48:51 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 624 ms, Memory: 6568 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cs
@@ -0,0 +1,31 @@
+int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = n; j > i + 1; --j)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int j = 0; j <= i; ++j)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i > 0; --i)
+            {
+                for (int j = n - 1; j > i - 1; --j)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int j = 0; j < i; ++j)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
