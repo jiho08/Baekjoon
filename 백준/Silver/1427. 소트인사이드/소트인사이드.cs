058a191dbac72dd81e1ff966b39ce8fe3864f9eb
박지호 <57872222k@gmail.com>
2024-08-24 18:58:28 +0900

    [Silver V] Title: 소트인사이드, Time: 48 ms, Memory: 5412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.cs
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cs
@@ -1,13 +1,7 @@
-string s = Console.ReadLine();
-            int[] n = new int[s.Length];
+char[] c = Console.ReadLine().ToCharArray();
 
+            Array.Sort(c);
+            Array.Reverse(c);
 
-            for (int i = 0; i < s.Length; ++i)
-                n[i] = int.Parse(s[i].ToString());
-
-            Array.Sort(n);
-
-            Array.Reverse(n);
-
-            foreach (int t in n)
-                Console.Write(t);
+            foreach (char i in c)
+                Console.Write(i);
