d54a3e0da3d62bd5f22f93b32009d0ba13652ee9
박지호 <57872222k@gmail.com>
2024-09-06 00:35:24 +0900

    Update 상근날드.cc

--- 백준/Bronze/5543. 상근날드/상근날드.cc
+++ 백준/Bronze/5543. 상근날드/상근날드.cc
@@ -1,27 +1,22 @@
-#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <vector>
+using namespace std;
 
-int main(void){
-    int L,M,S;
-    int coke, cider;
-    int bs, ds;
-    scanf("%d\n %d\n %d\n %d\n %d", &L, &M, &S, &coke, &cider);
-    if(L>=M){
-        bs=M;
-        if(bs>=S){
-            bs=S;
-        }
-    }
-    else if(L<M){
-        bs=L;
-        if(bs>=S){
-            bs=S;
-        }
-    }
-    if(coke<cider){
-        ds=coke;
-    }
-    else if(coke>=cider){
-        ds=cider;
-    }
-    printf("%d", bs+ds-50);
+int main()
+{
+	int a, b, c, d, e, x, y;
+	cin >> a >> b >> c >> d >> e;
+
+	x = a >= b ? b : a;
+	x = x >= c ? c : x;
+
+	y = d >= e ? e : d;
+
+	cout << x + y - 50;
+
+	return 0;
 }
