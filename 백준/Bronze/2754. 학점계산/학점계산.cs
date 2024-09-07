c64b37115bd3b92bed95b858238830195bf8e152
박지호 <57872222k@gmail.com>
2024-09-08 03:31:12 +0900

    [Bronze III] Title: 학점계산, Time: 44 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cs
@@ -0,0 +1,56 @@
+string s = Console.ReadLine();
+
+            switch (s)
+            {
+                case "A+":
+                    Console.WriteLine("4.3");
+                    break;
+
+                case "A0":
+                    Console.WriteLine("4.0");
+                    break;
+
+                case "A-":
+                    Console.WriteLine("3.7");
+                    break;
+
+                case "B+":
+                    Console.WriteLine("3.3");
+                    break;
+
+                case "B0":
+                    Console.WriteLine("3.0");
+                    break;
+
+                case "B-":
+                    Console.WriteLine("2.7");
+                    break;
+
+                case "C+":
+                    Console.WriteLine("2.3");
+                    break;
+
+                case "C0":
+                    Console.WriteLine("2.0");
+                    break;
+
+                case "C-":
+                    Console.WriteLine("1.7");
+                    break;
+
+                case "D+":
+                    Console.WriteLine("1.3");
+                    break;
+
+                case "D0":
+                    Console.WriteLine("1.0");
+                    break;
+
+                case "D-":
+                    Console.WriteLine("0.7");
+                    break;
+
+                case "F":
+                    Console.WriteLine("0.0");
+                    break;
+            }
