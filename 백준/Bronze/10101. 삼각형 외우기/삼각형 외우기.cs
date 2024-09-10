79194a0ee5a9027d139321285b2f8d2d4757e5f3
박지호 <57872222k@gmail.com>
2024-09-11 08:39:26 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 48 ms, Memory: 5464 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cs
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cs
@@ -1,19 +1,12 @@
-int a = int.Parse(Console.ReadLine());
-        int b = int.Parse(Console.ReadLine());
-        int c = int.Parse(Console.ReadLine());
-        if (a == 60 && a == b && b == c)
-        {
-            Console.WriteLine("Equilateral");
-        }
-        else if (a + b + c == 180 && a == b || a == c || b == c)
-        {
-            Console.WriteLine("Isosceles");
-        }
-        else if (a + b + c == 180)
-        {
-            Console.WriteLine("Scalene");
-        }
-        else
-        {
-            Console.WriteLine("Error");
-        }
+byte a = byte.Parse(Console.ReadLine());
+            byte b = byte.Parse(Console.ReadLine());
+            byte c = byte.Parse(Console.ReadLine());
+
+            if (a == 60 && a == b && a == c)
+                Console.WriteLine("Equilateral");
+            else if (a + b + c == 180 && (a == b || a == c || b == c))
+                Console.WriteLine("Isosceles");
+            else if (a + b + c == 180)
+                Console.WriteLine("Scalene");
+            else
+                Console.WriteLine("Error");
