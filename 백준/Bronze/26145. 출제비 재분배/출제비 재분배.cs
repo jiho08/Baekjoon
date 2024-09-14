e42a1ddfbe705c2ea0bdd425d8e33fffd0a85c31
박지호 <57872222k@gmail.com>
2024-09-15 05:36:29 +0900

    [Bronze III] Title: 출제비 재분배, Time: 276 ms, Memory: 27820 KB -BaekjoonHub

--- 백준/Bronze/26145. 출제비 재분배/출제비 재분배.cs
+++ 백준/Bronze/26145. 출제비 재분배/출제비 재분배.cs
@@ -1,22 +1,32 @@
-var input = Console.ReadLine()!.Split(' ');
-      var n = int.Parse(input[0]);
-      var m = int.Parse(input[1]);
+StreamWriter w = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            StreamReader r = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
 
-      var money = new int[n + m + 1];
+            int[] o = Array.ConvertAll(r.ReadLine().Split(), int.Parse);
+            int[] s = Array.ConvertAll(r.ReadLine().Split(), int.Parse);
 
-      input = Console.ReadLine()!.Split(' ');
-      for (int i = 1; i <= n; i++)
-        money[i] = int.Parse(input[i - 1]);
+            int[] m = new int[o[0] + o[1]];
+            int[,] f = new int[o[0], o[0] + o[1]];
 
-      for (int i = 1; i <= n; i++) {
-        input = Console.ReadLine()!.Split(' ');
-        for (int j = 1; j <= n + m; j++) {
-          var tmp = int.Parse(input[j - 1]);
-          money[i] -= tmp;
-          money[j] += tmp;
-        }
-      }
+            for (int i = 0; i < o[0]; ++i)
+                m[i] = s[i];
 
-      for (int i = 1; i <= n + m; i++)
-        Console.Write(money[i] + " ");
-      Console.WriteLine();
+            for (int i = 0; i < o[0]; ++i)
+            {
+                int[] x = Array.ConvertAll(r.ReadLine().Split(), int.Parse);
+
+                for (int j = 0; j < o[0] + o[1]; ++j)
+                    f[i, j] = x[j];
+            }
+
+            for (int i = 0; i < o[0]; ++i)
+                for (int j = 0; j < o[0] + o[1]; ++j)
+                {
+                    m[i] -= f[i, j];
+                    m[j] += f[i, j];
+                }
+
+            for (int i = 0; i < o[0] + o[1]; ++i)
+                w.Write($"{m[i]} ");
+
+            w.Close();
+            r.Close();
