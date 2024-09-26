ff770c17873723f20df688654613505d3347d6ce
박지호 <57872222k@gmail.com>
2024-09-26 23:40:21 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << endl
+    << "N2 Bomber      Heavy Fighter  Limited    21        " << endl   
+    << "J-Type 327     Light Combat   Unlimited  1         " << endl
+    << "NX Cruiser     Medium Fighter Limited    18        " << endl
+    << "N1 Starfighter Medium Fighter Unlimited  25        " << endl
+    << "Royal Cruiser  Light Combat   Limited    4        " << endl;
+    return 0;
+}
