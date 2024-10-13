c5bb9f829c3bf1c741b0af3cb864f15a080685af
박지호 <57872222k@gmail.com>
2024-10-14 00:02:26 +0900

    [Bronze II] Title: 자기복제수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2028. 자기복제수/자기복제수.cc
+++ 백준/Bronze/2028. 자기복제수/자기복제수.cc
@@ -1,14 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
 using namespace std;
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        string stringN = to_string(n);
-        string stringSquare = to_string(n*n);
-        if(stringSquare.find(stringN) == stringSquare.size() - stringN.size()) cout << "YES\n";
-        else cout << "NO\n";
-    }
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		string a = to_string(n);
+		string b = to_string(n * n);
+
+		if (b.find(a) == b.size() - a.size())
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
 }
