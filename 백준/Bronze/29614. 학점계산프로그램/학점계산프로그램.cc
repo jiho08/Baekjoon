08d5faa9f8e04eaba04d1ae48a1e01ddceefb3a6
박지호 <57872222k@gmail.com>
2024-10-27 23:21:44 +0900

    [Bronze II] Title: 학점계산프로그램, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29614. 학점계산프로그램/학점계산프로그램.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	float c = 0;
+	int q = 0;
+
+	string s;
+	cin >> s;
+
+	char r[s.size()] = {};
+
+	for (int i = 0; i < s.size(); ++i)
+		r[i] = s[i];
+
+	for (char t : r)
+		if (t == '+')
+			c += 0.5f;
+		else if (t >= 'A' && t < 'F')
+		{
+			c += 69 - t;
+			++q;
+		}
+		else if (t == 'F')
+		{
+			c += 0;
+			++q;
+		}
+
+		cout << c / q;
+}
