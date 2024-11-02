15f87ac66971d2f05e1ed2ea2ea26eb4cae14925
박지호 <57872222k@gmail.com>
2024-11-02 23:36:42 +0900

    [Bronze III] Title: 미션 도네이션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25965. 미션 도네이션/미션 도네이션.cc
+++ 백준/Bronze/25965. 미션 도네이션/미션 도네이션.cc
@@ -1,28 +1,31 @@
 #include <iostream>
+
 using namespace std;
 
-long long r[100][3];
-long long k, d, a;
-
-void solve() {
-	long long s = 0;
-	
-	int m; cin >> m;
-	for (int i = 0; i < m; ++i)
-        cin >> r[i][0] >> r[i][1] >> r[i][2];
-	
-	cin >> k >> d >> a;
-
-	for (int i = 0; i < m; i++)
-        s += max(r[i][0] * k - r[i][1] * d + r[i][2] * a, 0LL);
-
-	cout << s << '\n';
-}
-
-int main() {
+int main()
+{
 	ios::sync_with_stdio(0);
 
+	long long r[100][3];
+	long long k, d, a;
+
 	int n; cin >> n;
+
 	while (n--)
-        solve();
+	{
+		long long s = 0;
+
+		int m;
+		cin >> m;
+
+		for (int i = 0; i < m; ++i)
+			cin >> r[i][0] >> r[i][1] >> r[i][2];
+
+		cin >> k >> d >> a;
+
+		for (int i = 0; i < m; i++)
+			s += max(r[i][0] * k - r[i][1] * d + r[i][2] * a, 0LL);
+
+		cout << s << '\n';
+	}
 }
