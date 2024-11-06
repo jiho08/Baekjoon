9decb00730e61bb1656796577b755cfac6d856f7
박지호 <57872222k@gmail.com>
2024-11-06 15:49:36 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.cc
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.cc
@@ -2,17 +2,14 @@
 
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+int main()
+{
+	ios::sync_with_stdio(0);
 
-    n %= 8;
-    
-    if (n == 1) cout << 1;
-    else if (n == 2 || n == 0) cout << 2;
-    else if (n == 3 || n == 7) cout << 3;
-    else if (n == 4 || n == 6) cout << 4;
-    else if (n == 5) cout << 5;
-    
-    return 0;
+	int n;
+	cin >> n;
+
+	n %= 8;
+
+	cout << (n > 5 || n == 0 ? (10 - n) % 8 : n);
 }
