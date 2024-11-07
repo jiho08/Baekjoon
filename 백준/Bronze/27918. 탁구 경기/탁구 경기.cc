0d74002ff74dc98fa40531a4bf111a22b7b74149
박지호 <57872222k@gmail.com>
2024-11-07 10:07:00 +0900

    [Bronze IV] Title: 탁구 경기, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27918. 탁구 경기/탁구 경기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+
+	int n, d = 0, p = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		string s;
+		cin >> s;
+
+		s == "D" ? ++d : ++p;
+
+		if (abs(d - p) > 1)
+			break;
+	}
+
+	cout << d << ':' << p;
+}
