5345cb95e95a377ab4b870c17b01760b1b1fdc50
박지호 <57872222k@gmail.com>
2024-11-22 23:17:32 +0900

    [Bronze I] Title: A/B - 2, Time: 0 ms, Memory: 2020 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.cc
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	long double a, b;
+	cin >> a >> b;
+
+	cout << a / b;
+}
