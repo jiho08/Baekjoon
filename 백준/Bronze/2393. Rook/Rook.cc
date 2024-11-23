50c8889fd515fb4fa930f8a0bd5b42c3e248559e
박지호 <57872222k@gmail.com>
2024-11-23 16:40:37 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << "  ___  ___  ___\n"
+		<< "  | |__| |__| |\n"
+		<< "  |           |\n"
+		<< "   \\_________/\n"
+		<< "    \\_______/\n"
+		<< "     |     |\n"
+		<< "     |     |\n"
+		<< "     |     |\n"
+		<< "     |     |\n"
+		<< "     |_____|\n"
+		<< "  __/       \\__\n"
+		<< " /             \\\n"
+		<< "/_______________\\"
+		;
+}
