9df72036c78eb2d91817e91036dc4ab3f8385f56
박지호 <57872222k@gmail.com>
2024-11-24 23:32:45 +0900

    [Bronze V] Title: Serca, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	while (n--)
+	{
+		cout << " @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n";
+	}
+}
