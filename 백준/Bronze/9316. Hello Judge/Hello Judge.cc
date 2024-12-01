71f1d06336d7d8c6774e8d85533a7cd3f7f11a9b
박지호 <57872222k@gmail.com>
2024-12-01 23:13:10 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+		cout << "Hello World, Judge " << i << '!' << '\n';
+}
