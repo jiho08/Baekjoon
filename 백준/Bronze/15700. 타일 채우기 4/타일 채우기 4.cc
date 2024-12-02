a01bee69c38c287434f42a800d58e5080b3407f5
박지호 <57872222k@gmail.com>
2024-12-02 13:17:20 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	long n, m;
+	cin >> n >> m;
+
+	cout << n * m / 2;
+}
