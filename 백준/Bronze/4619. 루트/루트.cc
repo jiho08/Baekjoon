d6703094d9405cb207fcb545a1c35ba92307a0f8
박지호 <57872222k@gmail.com>
2025-01-05 00:01:33 +0900

    [Bronze III] Title: 루트, Time: 4 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/4619. 루트/루트.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath> // abs, pow
+using namespace std;
+
+int main() {
+    while (true) {
+        int b, p;
+        cin >> b >> p;
+        if (b == 0 && p == 0) break;
+
+        int a = 1;
+        while (pow(a, p) < b) {
+            a++;
+        }
+
+        int lower = pow(a - 1, p);
+        int upper = pow(a, p);
+
+        if (abs(b - lower) <= abs(b - upper)) {
+            cout << a - 1 << endl;
+        } else {
+            cout << a << endl;
+        }
+    }
+
+    return 0;
+}
