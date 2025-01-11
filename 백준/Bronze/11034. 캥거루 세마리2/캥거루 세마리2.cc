e289ec29666851b341b741cf4d79cd798153dd38
박지호 <57872222k@gmail.com>
2025-01-11 23:21:12 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    while (true)
+    {
+        int a, b, c, count = 0;
+        
+        if (!(cin >> a >> b >> c))
+            break;
+
+        while (true)
+        {
+            const int x = abs(a - b);
+            const int y = abs(b - c);
+
+            if (x == 1 && y == 1)
+                break;
+
+            if (x > y)
+            {
+                c = b;
+                --b;
+                ++count;
+            }
+            else
+            {
+                a = b;
+                ++b;
+                ++count;
+            }
+        }
+
+        cout << count << '\n';
+    }
+}
