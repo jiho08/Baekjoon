2255b1153bd3e8d853287de964bb4ee47c84e3ec
박지호 <57872222k@gmail.com>
2025-01-14 23:50:40 +0900

    [Bronze III] Title: 갈래의 색종이 자르기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/31472. 갈래의 색종이 자르기/갈래의 색종이 자르기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int w;
+	cin >> w;
+	cout << sqrt(w * 2) * 4;
+}
