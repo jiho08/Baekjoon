131ff4b04ced99b8e8bc9641bf2fa8c2b16f7f59
박지호 <57872222k@gmail.com>
2025-02-17 23:22:16 +0900

    [Bronze IV] Title: 글로벌 포닉스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31775. 글로벌 포닉스/글로벌 포닉스.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	bool a = false, b = false, c = false;
+	string r[3];
+	cin >> r[0] >> r[1] >> r[2];
+
+	for (auto& i : r)
+		if (i[0] == 'l')
+			a = true;
+
+	for (auto& i : r)
+		if (i[0] == 'k')
+			b = true;
+
+	for (auto& i : r)
+		if (i[0] == 'p')
+			c = true;
+
+	cout << (a == true && b == true && c == true ? "GLOBAL" : "PONIX");
+}
