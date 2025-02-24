6c18747cf9988986244fa3c7793c463d66240702
박지호 <57872222k@gmail.com>
2025-02-25 00:06:36 +0900

    [Bronze IV] Title: 구구단표, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/32710. 구구단표/구구단표.cc
+++ 백준/Bronze/32710. 구구단표/구구단표.cc
@@ -1,20 +1,20 @@
 #include <iostream>
+
 using namespace std;
 
-int N;
-bool visited[101];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	bool b = false;
+	int n;
+	cin >> n;
 
-	cin >> N;
-	for (int i = 1; i < 10; i++) {
-		visited[i] = 1;
-		for (int j = 1; j < 10; j++) {
-			visited[i * j] = 1;
-		}
-	}
-	if (visited[N]) cout << 1;
-	else cout << 0;
+	for (int i = 1; i < 10; ++i)
+		if (n % i == 0 && n / i <= 9)
+			b = true;
+
+	cout << (b ? 1 : 0);
 }
