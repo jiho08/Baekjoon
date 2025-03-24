1cbd92cf53ac1c315a12e3c98b73ab5b3ed7c667
박지호 <57872222k@gmail.com>
2025-03-24 23:39:43 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+#include <list>
+#include <string>
+#include <set>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int BinarySearch(int* arr, int t, int n)
+{
+	int left = 0, right = n - 1;
+
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+
+		if (arr[mid] == t)
+			return 1;
+
+		arr[mid] < t ? left = mid + 1 : right = mid - 1;
+	}
+
+	return 0;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, m;
+	cin >> n;
+
+	int* a = new int[n];
+
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+
+	cin >> m;
+
+	int* b = new int[m];
+
+	for (int i = 0; i < m; ++i)
+		cin >> b[i];
+
+	sort(a, a + n);
+
+	for (int i = 0; i < m; ++i)
+		cout << BinarySearch(a, b[i], n) << '\n';
+}
