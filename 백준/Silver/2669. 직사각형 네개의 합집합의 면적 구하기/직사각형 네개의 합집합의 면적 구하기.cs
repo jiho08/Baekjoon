5198af4b807898e5ae0eca37f27ca09a7a106afc
박지호 <57872222k@gmail.com>
2025-04-03 22:25:43 +0900

    Update 직사각형 네개의 합집합의 면적 구하기.cs

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.cs
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.cs
@@ -1,42 +1,17 @@
-int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            int[] b = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            int[] c = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            int[] d = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+var f = new bool[100, 100];
 
-            int[,] f = new int[100, 100];
-            
-            int cnt = 0;
+for (int i = 0; i < 4; i++)
+{
+	int[] r = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	FillSquare(r, f);
+}
 
-            for (int i = a[0]; i < a[2]; i++)
-            {
-                for (int j = a[1]; j < a[3]; j++)
-                    f[i, j] = 1;
-            }
+int cnt = f.Cast<bool>().Count(v => v);
+Console.WriteLine(cnt);
 
-            for (int i = b[0]; i < b[2]; i++)
-            {
-                for (int j = b[1]; j < b[3]; j++)
-                    f[i, j] = 1;
-            }
-
-            for (int i = c[0]; i < c[2]; i++)
-            {
-                for (int j = c[1]; j < c[3]; j++)
-                    f[i, j] = 1;
-            }
-
-            for (int i = d[0]; i < d[2]; i++)
-            {
-                for (int j = d[1]; j < d[3]; j++)
-                    f[i, j] = 1;
-            }
-
-            for (int i = 0; i < 100; i++)
-            {
-                for (int j = 0; j < 100; j++)
-                {
-                    if (f[i, j] == 1)
-                        cnt++;
-                }
-            }
-            Console.Write(cnt);
+static void FillSquare(int[] r, bool[,] f)
+{
+    for (int i = r[0]; i < r[2]; i++)
+        for (int j = r[1]; j < r[3]; j++)
+	        f[i, j] = true;
+}
