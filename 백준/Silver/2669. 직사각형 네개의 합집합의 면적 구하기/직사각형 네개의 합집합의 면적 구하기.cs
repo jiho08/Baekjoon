7cbe13c8f2b4529886ebb815876eb1c1f0739668
박지호 <57872222k@gmail.com>
2025-04-03 22:45:36 +0900

    Update 직사각형 네개의 합집합의 면적 구하기.cs

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.cs
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.cs
@@ -1,17 +1,17 @@
 var f = new bool[100, 100];
 
-for (int i = 0; i < 4; i++)
+for (int i = 0; i < 4; ++i)
 {
 	int[] r = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	FillSquare(r, f);
 }
 
 int cnt = f.Cast<bool>().Count(v => v);
-Console.WriteLine(cnt);
+Console.Write(cnt);
 
 static void FillSquare(int[] r, bool[,] f)
 {
-    for (int i = r[0]; i < r[2]; i++)
-        for (int j = r[1]; j < r[3]; j++)
+    for (int i = r[0]; i < r[2]; ++i)
+        for (int j = r[1]; j < r[3]; ++j)
 	        f[i, j] = true;
 }
