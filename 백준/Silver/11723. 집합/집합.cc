5b273f5fc1f0fe4541b0c46699da614a15889439
박지호 <57872222k@gmail.com>
2025-05-02 10:28:20 +0900

    [Silver V] Title: 집합, Time: 568 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,49 +1,44 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    int m;
-    vector<bool> r(21, false);
-    cin >> m;
+	int m, x, b = 0;
+	string s;
+	cin >> m;
 
-    while (m--)
-    {
-        string s;
-        int n;
-        cin >> s;
+	while (m--)
+	{
+		cin >> s;
 
-        if (s == "add")
-        {
-            cin >> n;
-            r[n] = true;
-        }
-        else if (s == "remove")
-        {
-            cin >> n;
-            r[n] = false;
-        }
-        else if (s == "check")
-        {
-            cin >> n;
-            cout << (r[n] ? 1 : 0) << '\n';
-        }
-        else if (s == "toggle")
-        {
-            cin >> n;
-            r[n] = !r[n];
-        }
-        else if (s == "all")
-            for (int i = 0; i < 21; ++i)
-                r[i] = true;
-        else if (s == "empty")
-            for (int i = 0; i < 21; ++i)
-                r[i] = false;
-    }
+		if (s == "add")
+		{
+			cin >> x;
+			b |= 1 << x;
+		}
+		else if (s == "remove")
+		{
+			cin >> x;
+			b &= ~(1 << x);
+		}
+		else if (s == "check")
+		{
+			cin >> x;
+			cout << (b & (1 << x) ? 1 : 0) << '\n';
+		}
+		else if (s == "toggle")
+		{
+			cin >> x;
+			b ^= 1 << x;
+		}
+		else if (s == "all")
+			b = (1 << 21) - 1;
+		else if (s == "empty")
+			b = 0;
+	}
 }
