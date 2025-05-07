682849ff96341a74caa308220237a26dff1ff746
박지호 <57872222k@gmail.com>
2025-05-07 23:21:06 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n + 1);
+
+	v[1] = 0;
+
+	for (int i = 2; i <= n; ++i)
+	{
+		int temp = INT16_MAX;
+
+		if (i % 3 == 0)
+			temp = v[i / 3];
+
+		if (i % 2 == 0)
+			temp = min(temp, v[i / 2]);
+
+		temp = min(temp, v[i - 1]);
+
+		v[i] = temp + 1;
+	}
+
+	cout << v[n];
+}
