5dcc1c5d678f2b4e0acb4ce022d4fcc1e21eaf64
박지호 <57872222k@gmail.com>
2025-06-01 22:38:34 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	while (n--)
+	{
+		int t;
+		cin >> t;
+
+		cout << t << ' ' << t << '\n';
+	}
+}
