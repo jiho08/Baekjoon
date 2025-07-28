84be626ff1c5abf27967ff5f669c7de11027f28e
박지호 <57872222k@gmail.com>
2025-07-28 22:11:29 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 3944 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <list>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> graph(n + 1);
+	vector<int> inDegree(n + 1, 0);
+	queue<int> q;
+
+	while (m--)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		graph[a].push_back(b);
+		++inDegree[b];
+	}
+
+	for (int i = 1; i <= n; ++i)
+		if (inDegree[i] == 0)
+			q.push(i);
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (q.empty())
+			break;
+
+		const int f = q.front();
+		q.pop();
+		cout << f << ' ';
+
+		for (int x : graph[f])
+			if (--inDegree[x] == 0)
+				q.push(x);
+	}
+}
