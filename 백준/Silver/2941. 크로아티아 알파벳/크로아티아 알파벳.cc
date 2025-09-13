be6d74f54a412e1021657043adc8f2351cf97608
박지호 <57872222k@gmail.com>
2025-09-13 22:40:48 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	string s;
+	cin >> s;
+
+	int cnt = 0;
+
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if (s[i] == 'c')
+		{
+			if (s[i + 1] == '=' || s[i + 1] == '-')
+				++i;
+
+			++cnt;
+		}
+		else if (s[i] == 'd')
+		{
+			if (s[i + 1] == 'z' && s[i + 2] == '=')
+				i += 2;
+			else if (s[i + 1] == '-')
+				++i;
+
+			++cnt;
+		}
+		else if (s[i] == 'l' || s[i] == 'n')
+		{
+			if (s[i + 1] == 'j')
+				++i;
+
+			++cnt;
+		}
+		else if (s[i] == 's' || s[i] == 'z')
+		{
+			if (s[i + 1] == '=')
+				++i;
+
+			++cnt;
+		}
+		else
+			++cnt;
+	}
+
+	cout << cnt;
+}
