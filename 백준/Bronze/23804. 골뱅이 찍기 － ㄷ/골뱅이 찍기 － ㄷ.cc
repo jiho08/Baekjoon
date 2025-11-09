66badc466c3faaeeb6fd1053f651391ba12eb1f0
박지호 <57872222k@gmail.com>
2025-11-09 22:40:31 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n * 5; ++j)
+			cout << '@';
+
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n * 3; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+			cout << '@';
+
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n * 5; ++j)
+			cout << '@';
+
+		cout << '\n';
+	}
+}
