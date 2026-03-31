b9f27f67d79b807932eb5a22d6223636e98a5ab3
박지호 <57872222k@gmail.com>
2026-03-31 23:34:05 +0900

    [Bronze I] Title: 영화 평가, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/23278. 영화 평가/영화 평가.cc
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	int n, l, h;
+	cin >> n >> l >> h;
+
+	double sum = 0;
+	deque<int> dq;
+
+	for (int i = 0; i < n; ++i)
+	{
+		int t;
+		cin >> t;
+		dq.push_back(t);
+	}
+
+	sort(dq.begin(), dq.end());
+
+	for (int i = 0; i < l; ++i)
+		dq.pop_front();
+
+	for (int i = 0; i < h; ++i)
+		dq.pop_back();
+
+	while (!dq.empty())
+	{
+		sum += dq.front();
+		dq.pop_front();
+	}
+
+	cout << fixed;
+	cout.precision(10);
+	cout << sum / (n - l - h);
+}
